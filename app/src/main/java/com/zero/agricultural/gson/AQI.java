5f32c4659786b9e74f5f987228eae9691e14b186
superxuanzero <superxuanzero@163.com>
2019-04-07 20:28:27 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/zero/agricultural/gson/AQI.java
@@ -0,0 +1,11 @@
+package com.zero.agricultural.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
